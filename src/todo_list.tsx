c064bc943253ccfdd43653e7e0ccc039e3869a03
王波 <wangbo@vikadata.com>
2021-12-03 19:18:34 +0800

    fix: optimized writing

--- src/todo_list.tsx
+++ src/todo_list.tsx
@@ -1,75 +1,72 @@
-import { Box, Button, IconButton, TextInput } from '@vikadata/components';
+import { Box, Button, IconButton, TextInput, Typography } from '@vikadata/components';
 import { AddOutlined, CloseMiddleOutlined } from '@vikadata/icons';
 import { useCloudStorage, ViewPicker, FieldPicker, useRecords, useDatasheet, usePrimaryField } from '@vikadata/widget-sdk';
 import React, { useState } from 'react';
-import { Setting } from './setting';
 
 export const TodoList: React.FC = () => {
   const datasheet = useDatasheet();
-  const [viewId, setViewId] = useCloudStorage<string>('selectedViewId')
+  const [viewId, setViewId] = useCloudStorage<string>('selectedViewId');
   const records = useRecords(viewId);
   const [fieldId, setFieldId] = useCloudStorage<string>('selectedFieldId');
   const [recordInput, setRecordInput] = useState<string>();
   const primaryField = usePrimaryField();
   const addRecords = () => {
-    const fieldsMap = {[primaryField!.id]: recordInput}
+    const fieldsMap = { [primaryField!.id]: recordInput };
     if (!datasheet) {
-      return
+      return;
     }
-    const addRecordCheckResult =  datasheet.checkPermissionsForAddRecord(fieldsMap)
+    const addRecordCheckResult =  datasheet.checkPermissionsForAddRecord(fieldsMap);
     if (!addRecordCheckResult.acceptable) {
-      alert(addRecordCheckResult.message)
-      return
+      alert(addRecordCheckResult.message);
+      return;
     }
-    datasheet.addRecord(fieldsMap)
-  }
+    datasheet.addRecord(fieldsMap);
+  };
 
   const setRecord = (recordId: string, fieldMap) => {
     if (!datasheet) {
-      return
+      return;
     }
-    const setRecordCheckResult =  datasheet.checkPermissionsForSetRecord(recordId, fieldMap)
+    const setRecordCheckResult =  datasheet.checkPermissionsForSetRecord(recordId, fieldMap);
     if (!setRecordCheckResult.acceptable) {
-      alert(setRecordCheckResult.message)
-      return
+      alert(setRecordCheckResult.message);
+      return;
     }
-    datasheet.setRecord(recordId, fieldMap)
-  }
+    datasheet.setRecord(recordId, fieldMap);
+  };
 
   const deleteRecord = (recordId: string) => {
     if (!datasheet) {
-      return
+      return;
     }
-    const deleteRecordCheckResult =  datasheet.checkPermissionsForDeleteRecord(recordId)
+    const deleteRecordCheckResult =  datasheet.checkPermissionsForDeleteRecord(recordId);
     if (!deleteRecordCheckResult.acceptable) {
-      alert(deleteRecordCheckResult.message)
-      return
+      alert(deleteRecordCheckResult.message);
+      return;
     }
-    datasheet.deleteRecord(recordId)
-  }
+    datasheet.deleteRecord(recordId);
+  };
 
   const tasks = records.map(record => {
-    const cellValue = record.getCellValue(fieldId)
-    const curCellValue = typeof cellValue === 'boolean' ? cellValue : false
+    const cellValue = record.getCellValue(fieldId);
+    const curCellValue = typeof cellValue === 'boolean' ? cellValue : false;
     return <Box
       paddingX={3}
       paddingY={3}
+      borderBottom="1px solid lightgrey"
       display="flex"
       alignItems="center"
-      borderBottom="1px solid rgba(0, 0, 0, 0.1)"
-      flexWrap="wrap"
     >
-      <input type="checkbox" id="check1" checked={curCellValue} onClick={e => setRecord(record.recordId, {[fieldId]: !curCellValue})}/>
-      <div style={{padding: '0 8px', flex: 1}}><b>{record.title}</b></div>
-      <RecordDeleteButton recordId={record.recordId} onClick={deleteRecord}/>
+      <input type="checkbox" id="check1" checked={curCellValue} onClick={() => setRecord(record.recordId, { [fieldId]: !curCellValue })} />
+      <Box paddingX={2} flex={1}>{record.title}</Box>
+      <RecordDeleteButton recordId={record.recordId} onClick={deleteRecord} />
     </Box>
-  })
+  });
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <div style={{ flexGrow: 1, overflow: 'auto'}}>
+    <div style={{ overflow: 'auto', height: '100%' }}>
         <Box
-          padding={'16px 10px 10px'}
-          borderBottom='2px solid rgba(0, 0, 0, 0.1)'
+          padding="16px 10px 10px"
+          borderBottom="2px solid lightgrey"
         >
           <FormItem label="View">
             <ViewPicker viewId={viewId} onChange={option => setViewId(option.value)} />
@@ -85,28 +82,26 @@ export const TodoList: React.FC = () => {
           display="flex"
           alignItems="center"
         >
-          <TextInput style={{flex: 1}} size="small" onChange={e => setRecordInput(e.target.value)}/>
-          <Button style={{minWidth: 'fit-content', marginLeft: 8}} prefixIcon={<AddOutlined/>} color="primary" size="small" onClick={addRecords}>
+          <TextInput block size="small" onChange={e => setRecordInput(e.target.value)} />
+          <Button style={{ whiteSpace: 'nowrap', marginLeft: 8 }} prefixIcon={<AddOutlined />} color="primary" size="small" onClick={addRecords}>
             Add
           </Button>
         </Box>
-      </div>
-      <Setting />
     </div>
   );
 };
 
-const FormItem = ({label, children}) => {
+const FormItem = ({ label, children }) => {
   return (
-    <div style={{display: 'flex', flexDirection: 'column', marginBottom: 16}}>
-      <label style={{paddingBottom: 6, fontSize: 13, color: '#636363', fontWeight: 'bold'}}>{label}</label>
+    <Box paddingY={1}>
+      <Typography>{label}</Typography>
       {children}
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
 const RecordDeleteButton = ({ recordId, onClick }) => {
   return (
-    <IconButton icon={CloseMiddleOutlined} variant="default" onClick={() => onClick(recordId)}></IconButton>
-  )
-}
+    <IconButton icon={CloseMiddleOutlined} variant="default" onClick={() => onClick(recordId)} />
+  );
+};
